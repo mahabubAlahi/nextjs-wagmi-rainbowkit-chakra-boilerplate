401f26d301505cbecd82a888a03171401c97e41f
mahabubAlahi <32974385+mahabubAlahi@users.noreply.github.com>
2023-10-19 17:47:40 +0600

    Add contract event read feature
    - Add success and failure toast

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import {
   Input,
   Spacer,
   Text,
+  useToast,
 } from "@chakra-ui/react";
 import Navbar from "components/Navbar";
 import type { NextPage } from "next";
@@ -18,28 +19,45 @@ import {
   writeContract,
   readContract,
 } from "@wagmi/core";
-import { useAccount } from "wagmi";
+import { useAccount, useContractEvent } from "wagmi";
 import greeterABI from "../abis/Greeter.json";
 import { useEffect, useState } from "react";
-import { providers, Wallet, Contract } from "ethers";
+import { providers, Wallet, Contract, utils } from "ethers";
 import { Providers } from "../config/providers";
 import { ChainId } from "config/constants/chainId";
+import {
+  createPublicClient,
+  decodeEventLog,
+  http,
+  parseAbiItem,
+  decodeAbiParameters,
+} from "viem";
 
 const Home: NextPage = () => {
   const { address, isDisconnected } = useAccount();
+  const toast = useToast();
 
   const [greeter, setGreeter] = useState("");
   const [greeterValue, setGreeterValue] = useState("");
   const [loading, setLoading] = useState(false);
 
+  const greetingContractAddress =
+    process.env.NEXT_PUBLIC_GREET_CONTRACT_ADDRESS;
+
   const handleChange = (e: any) => {
     setGreeter(e.target.value);
   };
 
-  const getGreetingValue = async () => {
-    const greetingContractAddress =
-      process.env.NEXT_PUBLIC_GREET_CONTRACT_ADDRESS;
+  useContractEvent({
+    address: greetingContractAddress as `0x${string}`,
+    abi: greeterABI,
+    eventName: "SetGreeting",
+    listener(log) {
+      console.log(log);
+    },
+  });
 
+  const getGreetingValue = async () => {
     // const data: string = (await readContract({
     //   address: greetingContractAddress as `0x${string}`,
     //   abi: greeterABI,
@@ -77,12 +95,54 @@ const Home: NextPage = () => {
       });
 
       const { hash } = await writeContract(request);
-      await waitForTransaction({
+      const transactionReceipt = await waitForTransaction({
         hash,
       });
+      //console.log(transactionReceipt);
+
+      //Event handling
+      const iface = new utils.Interface(greeterABI);
+      const parsed = iface.parseLog(transactionReceipt.logs[0]);
+      console.log("parsed", parsed);
+
+      //Event handling using viem
+
+      const logs = decodeEventLog({
+        abi: greeterABI,
+        data: transactionReceipt.logs[0].data,
+        topics: transactionReceipt.logs[0].topics,
+        eventName: "SetGreeting",
+      });
+
+      type SetGreeting = {
+        greeting?: string;
+        setter?: string;
+      };
+
+      console.log(logs);
+
+      const argValue: SetGreeting = logs.args;
+
       setLoading((value) => !value);
-      alert(`Transaction confirmed with hash ${hash}!`);
+      //alert(`Transaction confirmed with hash ${hash}!`);
+
+      toast({
+        title: "Transaction completed.",
+        description: `Transaction confirmed with hash ${hash}!`,
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+      });
     } catch (error) {
+      setLoading((value) => !value);
+
+      toast({
+        title: "Transaction failed.",
+        description: error.message,
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
       console.log(error);
     }
   };
